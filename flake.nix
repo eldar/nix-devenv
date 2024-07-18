de546824afb0b96d5cfa6693ee9ea2e351c213f0
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2024-07-18 21:03:02 +0100

    many fixes included for the broken shell
    1. nixpkgs-unstable -> nixos-unstable
    2. remove stable nixos-24.05 - mixing packages can be dangerous
    3. set $SHELL to bashInteractive in shellHooks
    4. add flake.lock to git for reproducibility

--- flake.nix
+++ flake.nix
@@ -1,45 +1,41 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs =  {
     self,
     nixpkgs,
-    nixpkgs-stable,
   }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs-stable.legacyPackages.${system};
-    unstable = nixpkgs.legacyPackages.${system};
+    pkgs = nixpkgs.legacyPackages.${system};
 
-    stableInputs = with pkgs; [
-      curl
-      zsh
-      stow
-      git
-      ripgrep
-      lua
-      luarocks
-      tmux
-      less
-    ];
-    
-    unstableInputs = with unstable; [
-      neovim
-      lazygit
-      delta
-      helix
-    ];
   in
   {
    devShells.${system}.default =
     pkgs.mkShell
     {
-      buildInputs = stableInputs ++ unstableInputs;
+      buildInputs = with pkgs; [
+        bashInteractive
+        curl
+        zsh
+        stow
+        git
+        ripgrep
+        lua
+        luarocks
+        tmux
+        less
+        neovim
+        lazygit
+        delta
+        helix
+      ];
       # https://github.com/NixOS/nix/issues/6677
+      # https://discourse.nixos.org/t/interactive-bash-with-nix-develop-flake/15486
       shellHook = ''
         export PS1='\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] '
+        SHELL=${pkgs.bashInteractive}/bin/bash
       '';
     };
   };
